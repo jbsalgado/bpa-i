75438c4cf19f7c0846341bd7abf1efaaee5f68f1
juniorpires <juniorpiresupe@gmail.com>
2012-08-06 13:50:47 -0300

    adicionando classe controller e DAO

---
+++ src/br/gov/saudecaruaru/bpai/business/controller/AcessoController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.business.controller;
+
+/**
+ *
+ * @author Junior Pires
+ */
+public class AcessoController {
+    
+}
