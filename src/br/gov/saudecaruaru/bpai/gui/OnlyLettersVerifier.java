2e52626ed540dc97b92b5926ac1ce05144c8eadf
juniorpires <juniorpiresupe@gmail.com>
2012-08-08 16:58:49 -0300

    Adicionando validadores paras os campos do cadastroIndividualizado

---
+++ src/br/gov/saudecaruaru/bpai/gui/OnlyLettersVerifier.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ * @author Junior Pires
+ */
+public class OnlyLettersVerifier extends InputVerifier{
+    private String fieldName; 
+    private Component component;
+
+    public OnlyLettersVerifier(Component component,String fieldName) {
+        this.fieldName = fieldName;
+        this.component = component;
+    }
+    
+    
+    @Override
+    public boolean verify(JComponent input) {
+       JTextComponent txtField = (JTextField) input; 
+      String valor = txtField.getText();
+       Pattern p = Pattern.compile("^[a-z,A-Z, ,.,ã,á,à,â,ê,í,ú,õ,é]+$");  
+                Matcher m = p.matcher(valor);  
+                if (!m.find()) {  
+                       JOptionPane.showMessageDialog(this.component,fieldName + " INCORRETO!"+" USE SOMENTE LETRAS", 
+                "Erro de validação!", JOptionPane.ERROR_MESSAGE); 
+                txtField.setBackground(Color.RED);
+                    return false;
+                }
+                  txtField.setBackground(Color.WHITE);
+                return true;
+       }
+    
+}
