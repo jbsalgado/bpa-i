db21fc2d50f37cf38bad1cbf0dd066f32b206025
juniorpires <juniorpiresupe@gmail.com>
2012-08-10 15:51:38 -0300

    adicionando DiversasDao

--- src/br/gov/saudecaruaru/bpai/data/DiversasDAO.java
+++ src/br/gov/saudecaruaru/bpai/data/DiversasDAO.java
@@ -7,6 +7,7 @@ package br.gov.saudecaruaru.bpai.data;
 import br.gov.saudecaruaru.bpai.business.model.Diversas;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.priuli.filter.Filter;
@@ -15,46 +16,8 @@ import net.priuli.filter.Filter;
  *
  * @author Junior Pires
  */
-public class DiversasDAO extends GenericDAO<Diversas> implements BasicDAO<Diversas> {
-
-    @Override
-    public void salve(Diversas object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Diversas encontre(Diversas object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void atualize(Diversas object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void exclua(Diversas object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public List<Diversas> encontreTodos() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public List<Diversas> encontreTodos(Filter filter) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Diversas encontre(Filter filter) {
-        try {
-            return super.find(filter);
-        } catch (Exception ex) {
-            Logger.getLogger(DiversasDAO.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
+public class DiversasDAO extends GenericDAO<Diversas> {
+ 
     
+  
 }
--- src/br/gov/saudecaruaru/bpai/main/BPAI.java
+++ src/br/gov/saudecaruaru/bpai/main/BPAI.java
@@ -5,11 +5,16 @@
 package br.gov.saudecaruaru.bpai.main;
 
 import br.gov.saudecaruaru.bpai.business.model.Acesso;
+import br.gov.saudecaruaru.bpai.business.model.AcessoPK;
+import br.gov.saudecaruaru.bpai.business.model.Diversas;
 import br.gov.saudecaruaru.bpai.business.model.Procedimento;
 import br.gov.saudecaruaru.bpai.data.AcessoDAO;
 import br.gov.saudecaruaru.bpai.data.BasicDAO;
+import br.gov.saudecaruaru.bpai.data.DiversasDAO;
 import br.gov.saudecaruaru.bpai.data.ProcedimentoDAO;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import net.priuli.filter.Filter;
 import net.priuli.filter.utils.FactoryFilter;
 
@@ -23,17 +28,14 @@ public class BPAI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BasicDAO<Acesso> ac=new AcessoDAO();
-        for(Acesso a: ac.encontreTodos()){
-             System.out.println(a.getAcessoPK().getUsername());
-        }
-        BasicDAO<Procedimento> t= new ProcedimentoDAO();
-        Filter filter= FactoryFilter.createFilter();
-        filter.addRestriction("procedimentoPk.competencia", "201206");
-        
-        List<Procedimento> l=t.encontreTodos(filter) ;
-        for(Procedimento a: l){
-            System.out.println(a.getDescricao()+"/"+a.getProcedimentoPk().getCompetencia()+'/'+a.getProcedimentoPk().getId());
+        BasicDAO<Diversas> t= new DiversasDAO();
+        HashMap<String, Object> rest=new HashMap<String, Object>();
+        rest.put("sCdnPK.cdnIt", "223605");
+        //rest.put("digitoVerificador", "3");
+        List<Diversas> l=t.findAllEqual(rest) ;
+        for(Diversas a: l){
+           // System.out.println(a.getDescricao()+"/"+a.getProcedimentoPk().getCompetencia()+'/'+a.getProcedimentoPk().getId());
+             System.out.println("Descrição: "+a.getCdnDscr());
         }
         System.out.println(l.size());
         
