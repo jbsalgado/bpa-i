9c79ebf831c29f36bd3e2da4bd8056534adaf167
cesao <cesar.consultorjr@gmail.com>
2012-08-10 14:46:11 -0300

    exemplo besta de filtragem

--- src/br/gov/saudecaruaru/bpai/main/BPAI.java
+++ src/br/gov/saudecaruaru/bpai/main/BPAI.java
@@ -9,8 +9,11 @@ import br.gov.saudecaruaru.bpai.business.model.Procedimento;
 import br.gov.saudecaruaru.bpai.data.AcessoDAO;
 import br.gov.saudecaruaru.bpai.data.BasicDAO;
 import br.gov.saudecaruaru.bpai.data.ProcedimentoDAO;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import net.priuli.filter.Filter;
+import net.priuli.filter.LikeMode;
 import net.priuli.filter.utils.FactoryFilter;
 
 /**
@@ -23,15 +26,12 @@ public class BPAI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BasicDAO<Acesso> ac=new AcessoDAO();
-        for(Acesso a: ac.encontreTodos()){
-             System.out.println(a.getAcessoPK().getUsername());
-        }
+      
         BasicDAO<Procedimento> t= new ProcedimentoDAO();
-        Filter filter= FactoryFilter.createFilter();
-        filter.addRestriction("procedimentoPk.competencia", "201206");
-        
-        List<Procedimento> l=t.encontreTodos(filter) ;
+        HashMap<String, Object> rest=new HashMap<String, Object>();
+        rest.put("procedimentoPk.competencia", "201206");
+        rest.put("digitoVerificador", "3");
+        List<Procedimento> l=t.findAllEqual(rest) ;
         for(Procedimento a: l){
             System.out.println(a.getDescricao()+"/"+a.getProcedimentoPk().getCompetencia()+'/'+a.getProcedimentoPk().getId());
         }
