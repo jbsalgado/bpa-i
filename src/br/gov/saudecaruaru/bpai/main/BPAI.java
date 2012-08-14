1ca4305ec439c878cdfb5eaf20d97a086818e91e
cesao <cesar.consultorjr@gmail.com>
2012-08-14 17:06:38 -0300

    modificacoes somente para testes

--- src/br/gov/saudecaruaru/bpai/main/BPAI.java
+++ src/br/gov/saudecaruaru/bpai/main/BPAI.java
@@ -6,7 +6,10 @@ package br.gov.saudecaruaru.bpai.main;
 
 import br.gov.saudecaruaru.bpai.business.model.*;
 import br.gov.saudecaruaru.bpai.data.*;
+import br.gov.saudecaruaru.bpai.util.ModelUtil;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -20,16 +23,18 @@ public class BPAI {
     public static void main(String[] args) {
         BasicDAO<Diversas> t= new DiversasDAO();
         Diversas d = new Diversas();
-        //d.setSCdnPK(new DiversasPK());
-        //d.getSCdnPK().setCdnIt("223605");
+        d.setDiversasPK(new DiversasPK());
+        d.getDiversasPK().setCodigoTabela(Diversas.TABELA_PROFISSAO);
         //d.setCdnDscr("Fisioterapeuta geral");
         //HashMap<String, Object> rest=new HashMap<String, Object>();
          ///rest.put("sCdnPK.cdnIt", "223605");
         //rest.put("digitoVerificador", "3");
         List<Diversas> l=t.findAllEqual(d) ;
-        for(Diversas a: l){
+        Map<String,String> m=ModelUtil.getMapSearch(l, "diversasPK.codigoItemTabela", "descricaoItemTabela");
+        for(String a: m.keySet()) {
            // System.out.println(a.getDescricao()+"/"+a.getProcedimentoPk().getCompetencia()+'/'+a.getProcedimentoPk().getId());
            //  System.out.println("Descrição: "+a.getCdnDscr()+"ID "+a.getSCdnPK().getCdnIt());
+           System.out.println("codigo CBO: "+a+"/"+m.get(a));
         }
  
         System.out.println(l.size());
