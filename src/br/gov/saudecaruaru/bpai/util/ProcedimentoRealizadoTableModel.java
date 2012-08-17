00f4720fa07fd80a74af350393fc46f9d1c9fed0
cesao <cesar.consultorjr@gmail.com>
2012-08-17 16:50:05 -0300

    classe utilizada como modelo de dados para uma JTable de objetos ProcedimentoRealizado. Implementa AbstractTableModel.

---
+++ src/br/gov/saudecaruaru/bpai/util/ProcedimentoRealizadoTableModel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.util;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Albuquerque
+ */
+public class ProcedimentoRealizadoTableModel extends AbstractTableModel{
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
