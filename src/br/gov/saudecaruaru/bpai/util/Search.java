2a20836b8e79e03dae6b588301e65b3d6072811f
cesao <cesar.consultorjr@gmail.com>
2012-08-17 16:47:22 -0300

    modelo base utilizado para pesquisa em geral (Tela SearchGeneric).

---
+++ src/br/gov/saudecaruaru/bpai/util/Search.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.util;
+
+/**
+ *
+ * @author Albuquerque
+ */
+public class Search {
+    
+    private String id;
+    private String description;
+
+    public Search() {
+    }
+
+    public Search(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Search other = (Search) obj;
+        if ((this.id == null) ? (other.id != null) : !this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+    
+    
+}
