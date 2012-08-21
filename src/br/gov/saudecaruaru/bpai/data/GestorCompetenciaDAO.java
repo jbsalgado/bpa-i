6776da9e211214298ac24e21c96715593c96c3f3
cesao <cesar.consultorjr@gmail.com>
2012-08-21 16:05:43 -0300

    DAO, Controller e Bean criados para o modelo de GestorCompetencia. Esse por sua vez, armazena informacoes referentes ao gestor do estabelecimento de saude e a competencia em questao!

---
+++ src/br/gov/saudecaruaru/bpai/data/GestorCompetenciaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.data;
+
+import br.gov.saudecaruaru.bpai.business.model.GestorCompetencia;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Albuquerque
+ */
+public class GestorCompetenciaDAO extends GenericDAO<GestorCompetencia> {
+    
+}
