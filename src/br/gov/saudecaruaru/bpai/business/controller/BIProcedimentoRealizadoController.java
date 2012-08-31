6a5e47078dfaf38f469879b67c86ea9949f45e65
juniorpires <juniorpiresupe@gmail.com>
2012-08-31 14:54:25 -0300

    criado metodo salvar no controlador BIProcedimentoRealizadoController que recebe uma List de procedimentoRealizado e salva-as no banco

--- src/br/gov/saudecaruaru/bpai/business/controller/BIProcedimentoRealizadoController.java
+++ src/br/gov/saudecaruaru/bpai/business/controller/BIProcedimentoRealizadoController.java
@@ -5,7 +5,9 @@
 package br.gov.saudecaruaru.bpai.business.controller;
 
 import br.gov.saudecaruaru.bpai.business.model.BIProcedimentoRealizado;
+import br.gov.saudecaruaru.bpai.business.model.ProcedimentoRealizado;
 import br.gov.saudecaruaru.bpai.data.BIProcedimentoRealizadoDAO;
+import java.util.List;
 
 /**
  *
@@ -15,6 +17,13 @@ public class BIProcedimentoRealizadoController extends BasecController<BIProcedi
 
     public BIProcedimentoRealizadoController() {
         super(new BIProcedimentoRealizadoDAO());
+    }
+    
+    
+    public void salvar(List<ProcedimentoRealizado> procedimentosRealizados){
+        for(ProcedimentoRealizado p : procedimentosRealizados){
+            salvar(new BIProcedimentoRealizado(p));
+        }
     }
     
     
