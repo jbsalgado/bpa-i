ac3fcfacd4f1ff50f5bcbf2ff63ec29bd87dccbe
cesao <cesar.consultorjr@gmail.com>
2012-09-08 14:55:43 -0300

    implementado a selecao de cabecalho: quando seleciona a linha traz todos os procedimentos que fazem parte da folha e quando recebe um clique duplo abre a janela de cadastro indivualizado ja com os dados preenchidos

--- src/br/gov/saudecaruaru/bpai/gui/ListaProcedimento.java
+++ src/br/gov/saudecaruaru/bpai/gui/ListaProcedimento.java
@@ -10,8 +10,14 @@
  */
 package br.gov.saudecaruaru.bpai.gui;
 
-import br.gov.saudecaruaru.bpai.data.ProcedimentoRealizadoDAO;
+import br.gov.saudecaruaru.bpai.business.controller.ProcedimentoRealizadoController;
+import br.gov.saudecaruaru.bpai.business.model.ProcedimentoRealizado;
+import br.gov.saudecaruaru.bpai.util.ProcedimentoRealizadoTableModelBody;
 import br.gov.saudecaruaru.bpai.util.ProcedimentoRealizadoTableModelHeader;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableColumnModel;
 
 /**
@@ -20,39 +26,110 @@ import javax.swing.table.TableColumnModel;
  */
 public class ListaProcedimento extends javax.swing.JDialog {
     
-    ProcedimentoRealizadoTableModelHeader tableModel;
-    ProcedimentoRealizadoDAO dao=new ProcedimentoRealizadoDAO();
-
+    private ProcedimentoRealizadoTableModelHeader tableModelHeader;
+    private ProcedimentoRealizadoTableModelBody tableModelBody;
+    private ProcedimentoRealizadoController procedimentoRealizadoController;
     /** Creates new form ListaProcedimento */
     public ListaProcedimento(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        initComponents();
+        //super(parent);
+        this.initComponents();
         this.myInitComponents();
     }
     
     private void myInitComponents(){
-        this.tableModel= new ProcedimentoRealizadoTableModelHeader(dao.findAllOnlyHeader());
-        this.jTable1.setModel(tableModel);
         
-        TableColumnModel columns=this.jTable1.getColumnModel();
-//        //coluna sequência
-//       columns.getColumn(0).setPreferredWidth(40);
-//       //coluna cns do paciente
-//       columns.getColumn(1).setPreferredWidth(120);
-//       //coluna nome do paciente
-//       columns.getColumn(2).setPreferredWidth(300);
-//       //coluna data de nascimento do paciente
-//       columns.getColumn(3).setPreferredWidth(70);
-//       //coluna sexo do paciente
-//       columns.getColumn(4).setPreferredWidth(40);
-//       //coluna município de residência do paciente
-//       columns.getColumn(5).setPreferredWidth(120);
-//       //coluna data de atendimento
-//       columns.getColumn(6).setPreferredWidth(100);
+        this.procedimentoRealizadoController= new ProcedimentoRealizadoController();
+        this.jcomboBoxFiltroActionPerformed(null);
+        this.initJTableBody();
+        this.initJTableHeader();
         
         
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
     }
 
+    /**
+     * Configura a tabela que armazena o cabeçalho
+     */
+    public void initJTableHeader(){
+        this.tableModelHeader= new ProcedimentoRealizadoTableModelHeader(this.procedimentoRealizadoController.findAllOnlyHeader());
+        this.jTableHeader.setModel(tableModelHeader);
+        this.jTableHeader.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        //quando a linha selecionada for mudada
+        this.jTableHeader.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                int row=ListaProcedimento.this.jTableHeader.getSelectedRow();
+                //tem linha selecionada
+                if(row>=0){
+                    
+                    //Pega o procedimento realizado de acordo com a linha selecionada na tabela
+                    ProcedimentoRealizado pro=ListaProcedimento.this.tableModelHeader.getProcedimentoRealizado(row);
+                    //zera a quantidade padrão
+                    pro.setQuantidadeRealizada(null);
+                    //substitui a lista
+                    ListaProcedimento.this.tableModelBody.replaceAllProcedimentoRealizado(ListaProcedimento.this.procedimentoRealizadoController.findAllEqual(pro));
+                }
+                //coloca uma lista vazia
+                else{
+                    ListaProcedimento.this.tableModelBody.replaceAllProcedimentoRealizado(new ArrayList<ProcedimentoRealizado>());
+                }
+               
+            }
+        });
+        TableColumnModel columns=this.jTableHeader.getColumnModel();
+       
+       //coluna CNES
+       columns.getColumn(0).setPreferredWidth(60);
+       //coluna competência
+       columns.getColumn(1).setPreferredWidth(110);
+       //coluna CNS do profissional de saúde
+       columns.getColumn(2).setPreferredWidth(130);
+       //coluna nome do profissional
+       columns.getColumn(3).setPreferredWidth(300);
+       //coluna CBO
+       columns.getColumn(4).setPreferredWidth(60);
+       //coluna folha
+       columns.getColumn(5).setPreferredWidth(50);
+       
+       //seleciona a primeira linha
+       if(!this.tableModelHeader.isEmpty()){
+            this.jTableHeader.setRowSelectionInterval(0, 0);
+       }
+    }
+    
+    /**
+     * Configura a tabela que armazena o corpo de procedimentoRealizado
+     */
+    private void initJTableBody(){
+        //inicia o modelo da tabela
+        this.tableModelBody=new ProcedimentoRealizadoTableModelBody(new ArrayList<ProcedimentoRealizado>());
+        this.jTableBody.setModel(this.tableModelBody);
+        this.jTableBody.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        
+       TableColumnModel columns=this.jTableBody.getColumnModel();
+        //coluna sequência
+       columns.getColumn(0).setPreferredWidth(40);
+       //coluna cns do paciente
+       columns.getColumn(1).setPreferredWidth(120);
+       //coluna nome do paciente
+       columns.getColumn(2).setPreferredWidth(300);
+       //coluna data de nascimento do paciente
+       columns.getColumn(3).setPreferredWidth(120);
+       //coluna sexo do paciente
+       columns.getColumn(4).setPreferredWidth(50);
+       //coluna município de residência do paciente
+       columns.getColumn(5).setPreferredWidth(120);
+       //coluna data de atendimento
+       columns.getColumn(6).setPreferredWidth(120);
+    }
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -63,19 +140,24 @@ public class ListaProcedimento extends javax.swing.JDialog {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTableHeader = new javax.swing.JTable();
         jcomboBoxFiltro = new javax.swing.JComboBox();
         jLabel1 = new javax.swing.JLabel();
         jTextFieldPesquisa = new javax.swing.JTextField();
         jbtnPesquisar = new javax.swing.JButton();
         jLabelTipoPesquisa = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
+        jTableBody = new javax.swing.JTable();
         jbtnIncluirFolha = new javax.swing.JButton();
         jbtnSair = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setAlwaysOnTop(true);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        jTableHeader.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -86,9 +168,14 @@ public class ListaProcedimento extends javax.swing.JDialog {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jTableHeader.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableHeaderMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTableHeader);
 
-        jcomboBoxFiltro.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jcomboBoxFiltro.setFont(new java.awt.Font("Tahoma", 0, 14));
         jcomboBoxFiltro.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "CNS DO PROFISSIONAL", "CNES", "COMPETÊNCIA", "CBO", "PROFISSIONAL" }));
         jcomboBoxFiltro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -96,23 +183,46 @@ public class ListaProcedimento extends javax.swing.JDialog {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel1.setText("Filtro de Pesquisa");
 
-        jTextFieldPesquisa.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jTextFieldPesquisa.setFont(new java.awt.Font("Tahoma", 0, 14));
         jTextFieldPesquisa.setToolTipText("");
 
-        jbtnPesquisar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jbtnPesquisar.setFont(new java.awt.Font("Tahoma", 0, 14));
         jbtnPesquisar.setText("Pesquisar");
 
         jLabelTipoPesquisa.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabelTipoPesquisa.setText("tipo");
 
+        jTableBody.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTableBody);
+
         jbtnIncluirFolha.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jbtnIncluirFolha.setText("Incluir folha");
+        jbtnIncluirFolha.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnIncluirFolhaMouseClicked(evt);
+            }
+        });
 
         jbtnSair.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jbtnSair.setText("Sair");
+        jbtnSair.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnSairMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -132,13 +242,13 @@ public class ListaProcedimento extends javax.swing.JDialog {
                                 .addGap(25, 25, 25)
                                 .addComponent(jbtnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(jLabelTipoPesquisa)))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 777, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 501, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 724, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jbtnIncluirFolha)
                         .addGap(483, 483, 483)
-                        .addComponent(jbtnSair)))
+                        .addComponent(jbtnSair))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 777, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -156,12 +266,11 @@ public class ListaProcedimento extends javax.swing.JDialog {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(41, 41, 41)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 267, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbtnIncluirFolha, javax.swing.GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
-                    .addComponent(jbtnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(11, 11, 11))
+                    .addComponent(jbtnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
         pack();
@@ -169,7 +278,35 @@ public class ListaProcedimento extends javax.swing.JDialog {
 
     private void jcomboBoxFiltroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcomboBoxFiltroActionPerformed
         // TODO add your handling code here:
+        this.jLabelTipoPesquisa.setText(this.jcomboBoxFiltro.getSelectedItem().toString());
     }//GEN-LAST:event_jcomboBoxFiltroActionPerformed
+
+    private void jbtnSairMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnSairMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jbtnSairMouseClicked
+
+    private void jbtnIncluirFolhaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnIncluirFolhaMouseClicked
+        // TODO add your handling code here:
+        new CadastroIndividualizado();
+    }//GEN-LAST:event_jbtnIncluirFolhaMouseClicked
+
+    private void jTableHeaderMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableHeaderMouseClicked
+        // TODO add your handling code here:
+        //evento de duplo clique
+        if(evt.getClickCount()==2){
+            int row=ListaProcedimento.this.jTableHeader.getSelectedRow();
+            //tem linha selecionada
+            if(row>=0){
+                CadastroIndividualizado cad=new CadastroIndividualizado();
+                ProcedimentoRealizado pro=this.tableModelHeader.getProcedimentoRealizado(row);
+                //zera a quantidade padrão
+                pro.setQuantidadeRealizada(null);
+                cad.findAllProcedimentosFolha(pro);
+            }
+            
+        }
+    }//GEN-LAST:event_jTableHeaderMouseClicked
 
     /**
      * @param args the command line arguments
@@ -219,7 +356,8 @@ public class ListaProcedimento extends javax.swing.JDialog {
     private javax.swing.JLabel jLabelTipoPesquisa;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTableBody;
+    private javax.swing.JTable jTableHeader;
     private javax.swing.JTextField jTextFieldPesquisa;
     private javax.swing.JButton jbtnIncluirFolha;
     private javax.swing.JButton jbtnPesquisar;
