56af900fbeaa48f34c73e4352270c3a9ace37761
cesao <cesar.consultorjr@gmail.com>
2012-09-11 15:48:34 -0300

    implementacao do metodo findAllOnlyHeader() que busca somente os procedimentos pelo cabecalho, ou seja, agrupa todos os procedimentos pelo cabecalho

--- src/br/gov/saudecaruaru/bpai/data/BIProcedimentoRealizadoDAO.java
+++ src/br/gov/saudecaruaru/bpai/data/BIProcedimentoRealizadoDAO.java
@@ -155,6 +155,57 @@ public class BIProcedimentoRealizadoDAO extends GenericDAO<BIProcedimentoRealiza
         return HibernateUtil.getSessionBpaI();
     }
     
-    
+    /**
+    * Devolve todos os procedimentos somente com os campos d cabeçalho preenchido.
+    * A saber: CNES da Unidade, CBO do profissional, Número da Folha, Competência e CNS do médico.
+    * Em outras palavaras: agrupa os resgistros pelos campos descritos acima.
+    * @return List<ProcedimentoRealizado> - A lista de todos os procedimentos encontrados
+    */
+   public List<ProcedimentoRealizado> findAllOnlyHeader(){
+        List<Object[]> l=null;
+        Session session= this.getSession();
+        try{
+
+            StringBuilder sql=new StringBuilder();
+            //campos a serem selecionados
+            sql.append("SELECT  pro.biProcedimentoRealizadoPK.cnesUnidade, pro.biProcedimentoRealizadoPK.cboMedico,");
+            sql.append(" pro.biProcedimentoRealizadoPK.numeroFolha, pro.biProcedimentoRealizadoPK.competencia,");
+            //faz o somatório da quantidade de execuções
+            sql.append("pro.biProcedimentoRealizadoPK.cnsMedico, COUNT(pro.biProcedimentoRealizadoPK.sequenciaFolha) AS VOID");
+            sql.append(" FROM BIProcedimentoRealizado pro");
+            //traz somente os procedimentos que devem ser consolidados
+            //sql.append(" WHERE (pro.origemProcedimento=:origem)");
+            //agrupa
+            sql.append(" GROUP BY pro.biProcedimentoRealizadoPK.competencia, pro.biProcedimentoRealizadoPK.cnesUnidade,");
+            sql.append(" pro.biProcedimentoRealizadoPK.cnsMedico,pro.biProcedimentoRealizadoPK.cboMedico,pro.biProcedimentoRealizadoPK.numeroFolha");
+            //it's create query
+            Query q=session.createQuery(sql.toString());
+            //paginacao dos resultados
+            l=q.list();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        finally{
+            List<ProcedimentoRealizado> list= new ArrayList<ProcedimentoRealizado>();
+            if(l!=null){
+                //cada objeto de l contém um vetor que representa os campos selecionados
+                for(Object[] row:l){
+                    ProcedimentoRealizado pro=new ProcedimentoRealizado(new ProcedimentoRealizadoPK());
+                    
+                    //o tamanho do vetor é igual a quantidade de campos do select
+                    //o índíce do campo no select é igual ao do vetor, começando por zero.
+                    pro.getProcedimentoRealizadoPK().setCnesUnidade((String)row[0]);
+                    pro.getProcedimentoRealizadoPK().setCboMedico((String)row[1]);
+                    pro.getProcedimentoRealizadoPK().setNumeroFolha((String)row[2]);
+                    pro.getProcedimentoRealizadoPK().setCompetencia((String)row[3]);
+                    pro.getProcedimentoRealizadoPK().setCnsMedico((String)row[4]);
+                    
+                    list.add(pro);
+                }
+            }
+            session.close();
+            return list;
+        }
+    }
     
 }
