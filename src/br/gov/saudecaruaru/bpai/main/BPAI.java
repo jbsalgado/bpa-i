5034182e2d0f427e41296ccb741d6a9890a756ee
juniorpires <juniorpiresupe@gmail.com>
2012-09-12 15:35:30 -0300

    implementado metodo que pega a proxima folha dado determinado registro,implementado auto completar de folha na tela de cadastro

--- src/br/gov/saudecaruaru/bpai/main/BPAI.java
+++ src/br/gov/saudecaruaru/bpai/main/BPAI.java
@@ -5,6 +5,7 @@
 package br.gov.saudecaruaru.bpai.main;
 
 import br.gov.saudecaruaru.bpai.business.controller.BIProcedimentoRealizadoController;
+import br.gov.saudecaruaru.bpai.business.controller.ProcedimentoRealizadoController;
 import br.gov.saudecaruaru.bpai.business.model.*;
 import br.gov.saudecaruaru.bpai.data.*;
 import br.gov.saudecaruaru.bpai.gui.SearchGeneric;
@@ -36,10 +37,22 @@ public class BPAI {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(SearchGeneric.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        ProcedimentoRealizado pr = new ProcedimentoRealizado();
         
-        BIProcedimentoRealizadoDAO dr=new BIProcedimentoRealizadoDAO();
-        for(BIProcedimentoRealizado p:dr.findAll()){
-            System.out.println(p);
-        }
+        BIProcedimentoRealizadoController prc = new BIProcedimentoRealizadoController();
+        
+        pr.getProcedimentoRealizadoPK().setCnesUnidade("3083721");
+        pr.getProcedimentoRealizadoPK().setCnsMedico("980016277313814");
+        pr.getProcedimentoRealizadoPK().setCboMedico("223505");
+        pr.getProcedimentoRealizadoPK().setCompetencia("201206");
+        
+        BIProcedimentoRealizado BIpr = new BIProcedimentoRealizado(pr);
+        String folha = prc.getNextFolha(BIpr);
+        System.out.println("Folha "+folha);
+        
+//        BIProcedimentoRealizadoDAO dr=new BIProcedimentoRealizadoDAO();
+//        for(BIProcedimentoRealizado p:dr.findAll()){
+//            System.out.println(p);
+//        }
     }
 }
