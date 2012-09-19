ec090214a8bd6ee465a8064a2cc057162c613a7b
cesao <cesar.consultorjr@gmail.com>
2012-09-19 14:25:41 -0300

    cricao da classe

---
+++ src/br/gov/saudecaruaru/bpai/business/controller/EquipeController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.business.controller;
+
+import br.gov.saudecaruaru.bpai.business.model.Equipe;
+import br.gov.saudecaruaru.bpai.data.EquipeDAO;
+
+/**
+ *
+ * @author Albuquerque
+ */
+public class EquipeController extends BasecController<Equipe>{
+
+    public EquipeController() {
+        super(new EquipeDAO());
+    }
+    
+}
