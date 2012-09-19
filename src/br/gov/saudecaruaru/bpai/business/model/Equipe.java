4dfb7a6ac8e684e8fa509dba8d870f9bbd4da8c0
cesao <cesar.consultorjr@gmail.com>
2012-09-19 14:27:39 -0300

    mapeamento dos campos para a tabela. Implementacao dos construtores, metodos getters e setters, entre outros.

--- src/br/gov/saudecaruaru/bpai/business/model/Equipe.java
+++ src/br/gov/saudecaruaru/bpai/business/model/Equipe.java
@@ -4,10 +4,140 @@
  */
 package br.gov.saudecaruaru.bpai.business.model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  *
  * @author Albuquerque
  */
-public class Equipe {
+@Entity
+@Table(name= "S_EQESF")
+public class Equipe implements Serializable{
+
+    @EmbeddedId
+    private EquipePK equipePK;
+    
+    @Column(name="ESF_AREA")
+    private String area;
+        
+    @Column(name="ESF_NOME")
+    private String nome;
+        
+    @Column(name="ESF_TIPO")
+    private String tipo;
+        
+    @Column(name="ESF_DT_VIGENCIA_INICIAL")
+    private String dataVigenciaInicial;
+            
+    @Column(name="ESF_DT_VIGENCIA_FINAL")
+    private String dataVigenciaFinal;
+    
+    @Column(name="ESF_AUX")
+    private Character auxiliar;
+
+    public Equipe() {
+    }
+
+    public Equipe(EquipePK equipePK) {
+        this.equipePK = equipePK;
+    }
+
+    public Equipe(EquipePK equipePK, String area, String nome, String tipo, String dataVigenciaInicial, String dataVigenciaFinal, Character auxiliar) {
+        this.equipePK = equipePK;
+        this.area = area;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.dataVigenciaInicial = dataVigenciaInicial;
+        this.dataVigenciaFinal = dataVigenciaFinal;
+        this.auxiliar = auxiliar;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Character getAuxiliar() {
+        return auxiliar;
+    }
+
+    public void setAuxiliar(Character auxiliar) {
+        this.auxiliar = auxiliar;
+    }
+
+    public String getDataVigenciaFinal() {
+        return dataVigenciaFinal;
+    }
+
+    public void setDataVigenciaFinal(String dataVigenciaFinal) {
+        this.dataVigenciaFinal = dataVigenciaFinal;
+    }
+
+    public String getDataVigenciaInicial() {
+        return dataVigenciaInicial;
+    }
+
+    public void setDataVigenciaInicial(String dataVigenciaInicial) {
+        this.dataVigenciaInicial = dataVigenciaInicial;
+    }
+
+    public EquipePK getEquipePK() {
+        return equipePK;
+    }
+
+    public void setEquipePK(EquipePK equipePK) {
+        this.equipePK = equipePK;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equipe other = (Equipe) obj;
+        if (this.equipePK != other.equipePK && (this.equipePK == null || !this.equipePK.equals(other.equipePK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + (this.equipePK != null ? this.equipePK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipe{" + "equipePK=" + equipePK + ", area=" + area + ", nome=" + nome + ", tipo=" + tipo + ", dataVigenciaInicial=" + dataVigenciaInicial + ", dataVigenciaFinal=" + dataVigenciaFinal + ", auxiliar=" + auxiliar + '}';
+    }
+    
     
 }
--- src/br/gov/saudecaruaru/bpai/business/model/EquipePK.java
+++ src/br/gov/saudecaruaru/bpai/business/model/EquipePK.java
@@ -4,10 +4,115 @@
  */
 package br.gov.saudecaruaru.bpai.business.model;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 /**
  *
  * @author Albuquerque
  */
-public class EquipePK {
+@Embeddable
+public class EquipePK implements Serializable{
+
+                      
+    @Basic(optional= false)
+    @Column(name="ESF_CMP")
+    private String competencia;
+    
+    @Basic(optional= false)
+    @Column(name="ESF_IBGE")
+    private String codigoIBGECidade;
+        
+    @Basic(optional= false)
+    @Column(name="ESF_CNES")
+    private String cnes;
+        
+    @Basic(optional= false)
+    @Column(name="ESF_SEQ")
+    private String sequencia;
+
+    public EquipePK() {
+    }
+
+    public EquipePK(String competencia, String codigoIBGECidade, String cnes, String sequencia) {
+        this.competencia = competencia;
+        this.codigoIBGECidade = codigoIBGECidade;
+        this.cnes = cnes;
+        this.sequencia = sequencia;
+    }
+
+    public String getCnes() {
+        return cnes;
+    }
+
+    public void setCnes(String cnes) {
+        this.cnes = cnes;
+    }
+
+    public String getCodigoIBGECidade() {
+        return codigoIBGECidade;
+    }
+
+    public void setCodigoIBGECidade(String codigoIBGECidade) {
+        this.codigoIBGECidade = codigoIBGECidade;
+    }
+
+    public String getCompetencia() {
+        return competencia;
+    }
+
+    public void setCompetencia(String competencia) {
+        this.competencia = competencia;
+    }
+
+    public String getSequencia() {
+        return sequencia;
+    }
+
+    public void setSequencia(String sequencia) {
+        this.sequencia = sequencia;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EquipePK other = (EquipePK) obj;
+        if ((this.competencia == null) ? (other.competencia != null) : !this.competencia.equals(other.competencia)) {
+            return false;
+        }
+        if ((this.codigoIBGECidade == null) ? (other.codigoIBGECidade != null) : !this.codigoIBGECidade.equals(other.codigoIBGECidade)) {
+            return false;
+        }
+        if ((this.cnes == null) ? (other.cnes != null) : !this.cnes.equals(other.cnes)) {
+            return false;
+        }
+        if ((this.sequencia == null) ? (other.sequencia != null) : !this.sequencia.equals(other.sequencia)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + (this.competencia != null ? this.competencia.hashCode() : 0);
+        hash = 53 * hash + (this.codigoIBGECidade != null ? this.codigoIBGECidade.hashCode() : 0);
+        hash = 53 * hash + (this.cnes != null ? this.cnes.hashCode() : 0);
+        hash = 53 * hash + (this.sequencia != null ? this.sequencia.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "EquipePK{" + "competencia=" + competencia + ", codigoIBGECidade=" + codigoIBGECidade + ", cnes=" + cnes + ", sequencia=" + sequencia + '}';
+    }
+    
     
 }
