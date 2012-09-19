f04e581ef2f0594d10fae507c58a60481b15b78b
cesao <cesar.consultorjr@gmail.com>
2012-09-19 14:25:52 -0300

    cricao da classe

---
+++ src/br/gov/saudecaruaru/bpai/data/EquipeDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.saudecaruaru.bpai.data;
+
+import br.gov.saudecaruaru.bpai.business.model.Equipe;
+
+/**
+ *
+ * @author Albuquerque
+ */
+public class EquipeDAO extends GenericDAO<Equipe> {
+    
+}
