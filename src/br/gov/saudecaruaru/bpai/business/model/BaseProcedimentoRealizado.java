7a0f1c88477f0738ae8d3c6b3a9ef4dbb54f0327
cesao <cesar.consultorjr@gmail.com>
2012-10-18 22:40:17 -0300

    refatoracao

--- src/br/gov/saudecaruaru/bpai/business/model/BaseProcedimentoRealizado.java
+++ src/br/gov/saudecaruaru/bpai/business/model/BaseProcedimentoRealizado.java
@@ -4,16 +4,20 @@
  */
 package br.gov.saudecaruaru.bpai.business.model;
 
+import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.Transient;
 
 /**
  *
  * @author Albuquerque
  */
-public class BaseProcedimentoRealizado {
+@MappedSuperclass
+public class BaseProcedimentoRealizado implements Serializable{
+    
     private static final long serialVersionUID = 1L;
     
     public static final String ORIGEM_CONSOLIDADO="BPA";
@@ -69,7 +73,7 @@ public class BaseProcedimentoRealizado {
     private String origemProcedimento;
     
     @Column(name = "PRD_MVM")
-    private String prdMvm;
+    private String movimentoCompetencia;
     
     @Column(name = "PRD_FLPA")
     private String prdFlpa;
@@ -103,4 +107,292 @@ public class BaseProcedimentoRealizado {
 
     @Transient
     private String nomeProfissional;
+
+    public BaseProcedimentoRealizado() {
+    }
+
+    public BaseProcedimentoRealizado(ProcedimentoRealizadoPK procedimentoRealizadoPK) {
+        this.procedimentoRealizadoPK = procedimentoRealizadoPK;
+    }
+
+    public BaseProcedimentoRealizado(ProcedimentoRealizadoPK procedimentoRealizadoPK, String codigoProcedimento, String dataAtendimento, String cnsPaciente, String nomePaciente, String dataNascimentoPaciente, String sexoPaciente, String codigoIBGECidadePaciente, String cidDoencaprocedimento, String idadePaciente, Double quantidadeRealizada, String caracterizacaoAtendimento, String numeroAutorizacao, String origemProcedimento, String movimentoCompetencia, String prdFlpa, String prdFlcbo, String prdFlca, String prdFlida, String prdFlqt, String prdFler, String prdFlmun, String prdFlcid, String racaPaciente, String etniaPaciente, String nacionalidadePaciente, String prdAdvqt, String nomeProfissional) {
+        this.procedimentoRealizadoPK = procedimentoRealizadoPK;
+        this.codigoProcedimento = codigoProcedimento;
+        this.dataAtendimento = dataAtendimento;
+        this.cnsPaciente = cnsPaciente;
+        this.nomePaciente = nomePaciente;
+        this.dataNascimentoPaciente = dataNascimentoPaciente;
+        this.sexoPaciente = sexoPaciente;
+        this.codigoIBGECidadePaciente = codigoIBGECidadePaciente;
+        this.cidDoencaprocedimento = cidDoencaprocedimento;
+        this.idadePaciente = idadePaciente;
+        this.quantidadeRealizada = quantidadeRealizada;
+        this.caracterizacaoAtendimento = caracterizacaoAtendimento;
+        this.numeroAutorizacao = numeroAutorizacao;
+        this.origemProcedimento = origemProcedimento;
+        this.movimentoCompetencia = movimentoCompetencia;
+        this.prdFlpa = prdFlpa;
+        this.prdFlcbo = prdFlcbo;
+        this.prdFlca = prdFlca;
+        this.prdFlida = prdFlida;
+        this.prdFlqt = prdFlqt;
+        this.prdFler = prdFler;
+        this.prdFlmun = prdFlmun;
+        this.prdFlcid = prdFlcid;
+        this.racaPaciente = racaPaciente;
+        this.etniaPaciente = etniaPaciente;
+        this.nacionalidadePaciente = nacionalidadePaciente;
+        this.prdAdvqt = prdAdvqt;
+        this.nomeProfissional = nomeProfissional;
+    }
+
+    
+    
+    public String getCaracterizacaoAtendimento() {
+        return caracterizacaoAtendimento;
+    }
+
+    public void setCaracterizacaoAtendimento(String caracterizacaoAtendimento) {
+        this.caracterizacaoAtendimento = caracterizacaoAtendimento;
+    }
+
+    public String getCidDoencaprocedimento() {
+        return cidDoencaprocedimento;
+    }
+
+    public void setCidDoencaprocedimento(String cidDoencaprocedimento) {
+        this.cidDoencaprocedimento = cidDoencaprocedimento;
+    }
+
+    public String getCnsPaciente() {
+        return cnsPaciente;
+    }
+
+    public void setCnsPaciente(String cnsPaciente) {
+        this.cnsPaciente = cnsPaciente;
+    }
+
+    public String getCodigoIBGECidadePaciente() {
+        return codigoIBGECidadePaciente;
+    }
+
+    public void setCodigoIBGECidadePaciente(String codigoIBGECidadePaciente) {
+        this.codigoIBGECidadePaciente = codigoIBGECidadePaciente;
+    }
+
+    public String getCodigoProcedimento() {
+        return codigoProcedimento;
+    }
+
+    public void setCodigoProcedimento(String codigoProcedimento) {
+        this.codigoProcedimento = codigoProcedimento;
+    }
+
+    public String getDataAtendimento() {
+        return dataAtendimento;
+    }
+
+    public void setDataAtendimento(String dataAtendimento) {
+        this.dataAtendimento = dataAtendimento;
+    }
+
+    public String getDataNascimentoPaciente() {
+        return dataNascimentoPaciente;
+    }
+
+    public void setDataNascimentoPaciente(String dataNascimentoPaciente) {
+        this.dataNascimentoPaciente = dataNascimentoPaciente;
+    }
+
+    public String getEtniaPaciente() {
+        return etniaPaciente;
+    }
+
+    public void setEtniaPaciente(String etniaPaciente) {
+        this.etniaPaciente = etniaPaciente;
+    }
+
+    public String getIdadePaciente() {
+        return idadePaciente;
+    }
+
+    public void setIdadePaciente(String idadePaciente) {
+        this.idadePaciente = idadePaciente;
+    }
+
+    public String getMovimentoCompetencia() {
+        return movimentoCompetencia;
+    }
+
+    public void setMovimentoCompetencia(String movimentoCompetencia) {
+        this.movimentoCompetencia = movimentoCompetencia;
+    }
+
+    public String getNacionalidadePaciente() {
+        return nacionalidadePaciente;
+    }
+
+    public void setNacionalidadePaciente(String nacionalidadePaciente) {
+        this.nacionalidadePaciente = nacionalidadePaciente;
+    }
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public String getNomeProfissional() {
+        return nomeProfissional;
+    }
+
+    public void setNomeProfissional(String nomeProfissional) {
+        this.nomeProfissional = nomeProfissional;
+    }
+
+    public String getNumeroAutorizacao() {
+        return numeroAutorizacao;
+    }
+
+    public void setNumeroAutorizacao(String numeroAutorizacao) {
+        this.numeroAutorizacao = numeroAutorizacao;
+    }
+
+    public String getOrigemProcedimento() {
+        return origemProcedimento;
+    }
+
+    public void setOrigemProcedimento(String origemProcedimento) {
+        this.origemProcedimento = origemProcedimento;
+    }
+
+    public String getPrdAdvqt() {
+        return prdAdvqt;
+    }
+
+    public void setPrdAdvqt(String prdAdvqt) {
+        this.prdAdvqt = prdAdvqt;
+    }
+
+    public String getPrdFlca() {
+        return prdFlca;
+    }
+
+    public void setPrdFlca(String prdFlca) {
+        this.prdFlca = prdFlca;
+    }
+
+    public String getPrdFlcbo() {
+        return prdFlcbo;
+    }
+
+    public void setPrdFlcbo(String prdFlcbo) {
+        this.prdFlcbo = prdFlcbo;
+    }
+
+    public String getPrdFlcid() {
+        return prdFlcid;
+    }
+
+    public void setPrdFlcid(String prdFlcid) {
+        this.prdFlcid = prdFlcid;
+    }
+
+    public String getPrdFler() {
+        return prdFler;
+    }
+
+    public void setPrdFler(String prdFler) {
+        this.prdFler = prdFler;
+    }
+
+    public String getPrdFlida() {
+        return prdFlida;
+    }
+
+    public void setPrdFlida(String prdFlida) {
+        this.prdFlida = prdFlida;
+    }
+
+    public String getPrdFlmun() {
+        return prdFlmun;
+    }
+
+    public void setPrdFlmun(String prdFlmun) {
+        this.prdFlmun = prdFlmun;
+    }
+
+    public String getPrdFlpa() {
+        return prdFlpa;
+    }
+
+    public void setPrdFlpa(String prdFlpa) {
+        this.prdFlpa = prdFlpa;
+    }
+
+    public String getPrdFlqt() {
+        return prdFlqt;
+    }
+
+    public void setPrdFlqt(String prdFlqt) {
+        this.prdFlqt = prdFlqt;
+    }
+
+    public ProcedimentoRealizadoPK getProcedimentoRealizadoPK() {
+        return procedimentoRealizadoPK;
+    }
+
+    public void setProcedimentoRealizadoPK(ProcedimentoRealizadoPK procedimentoRealizadoPK) {
+        this.procedimentoRealizadoPK = procedimentoRealizadoPK;
+    }
+
+    public Double getQuantidadeRealizada() {
+        return quantidadeRealizada;
+    }
+
+    public void setQuantidadeRealizada(Double quantidadeRealizada) {
+        this.quantidadeRealizada = quantidadeRealizada;
+    }
+
+    public String getRacaPaciente() {
+        return racaPaciente;
+    }
+
+    public void setRacaPaciente(String racaPaciente) {
+        this.racaPaciente = racaPaciente;
+    }
+
+    public String getSexoPaciente() {
+        return sexoPaciente;
+    }
+
+    public void setSexoPaciente(String sexoPaciente) {
+        this.sexoPaciente = sexoPaciente;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BaseProcedimentoRealizado other = (BaseProcedimentoRealizado) obj;
+        if (this.procedimentoRealizadoPK != other.procedimentoRealizadoPK && (this.procedimentoRealizadoPK == null || !this.procedimentoRealizadoPK.equals(other.procedimentoRealizadoPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.procedimentoRealizadoPK != null ? this.procedimentoRealizadoPK.hashCode() : 0);
+        return hash;
+    }
+    
+    
 }
