970f212513697b63c8bc3d662153a48c8fb5e561
cesao <cesar.consultorjr@gmail.com>
2013-02-05 10:52:30 -0300

    teste.desconsiderar

--- src/br/gov/saudecaruaru/bpai/main/Teste.java
+++ src/br/gov/saudecaruaru/bpai/main/Teste.java
@@ -58,9 +58,6 @@ import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import sun.org.mozilla.javascript.internal.json.JsonParser;
 /**
  *
@@ -243,12 +240,6 @@ public class Teste {
       // resorce.queryParam("t", "<inicio> valor</inicio>");
        //resorce.entity(new File("C:\\Users\\Albuquerque\\Desktop\\bck.xml"));
        String s=resorce.get(String.class);
-       JSONParser parser=new JSONParser();
-        try {
-            Object o=parser.parse(s);
-            JSONObject obj=(JSONObject) o;
-            System.out.println(obj.get("raca"));
-            System.out.println(obj);
            // System.out.println( resorce.get(String.class));
      //        try {
      //            URI url=new URI("http://localhost/sispad/index.php/bpa/procedimentorealizado/envio");
@@ -273,8 +264,5 @@ public class Teste {
      //        } catch (Exception ex) {
      //        }
      //        }
-        } catch (ParseException ex) {
-            java.util.logging.Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
-        }
    }
 }
