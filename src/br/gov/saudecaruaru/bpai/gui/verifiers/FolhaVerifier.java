c47d1564b17708df08add7fb6d147dcc14c86378
cesao <cesar.consultorjr@gmail.com>
2013-02-19 14:05:30 -0300

    vallidacao da folha reimplementada

--- src/br/gov/saudecaruaru/bpai/gui/verifiers/FolhaVerifier.java
+++ src/br/gov/saudecaruaru/bpai/gui/verifiers/FolhaVerifier.java
@@ -4,9 +4,11 @@
  */
 package br.gov.saudecaruaru.bpai.gui.verifiers;
 
+import br.gov.saudecaruaru.bpai.business.model.ProcedimentoRealizado;
 import br.gov.saudecaruaru.bpai.gui.MessagesErrors;
 import java.awt.Color;
 import java.awt.Component;
+import javax.swing.InputVerifier;
 import javax.swing.JComponent;
 import javax.swing.JTextField;
 
@@ -14,26 +16,47 @@ import javax.swing.JTextField;
  *
  * @author Junior Pires
  */
-  //classe validadora para o campo CNSUsuario
-public class FolhaVerifier extends CnsVerifier{
-        private Component component=null;
-        
-        public FolhaVerifier(Component component, String fieldName) {
-            super(component, fieldName);
-            this.component=component;
-            
+//classe validadora para o campo CNSUsuario
+public class FolhaVerifier extends InputVerifier {
+
+    private Component component = null;
+
+    public FolhaVerifier(Component component, String fieldName) {
+        this.component = component;
+
+    }
+
+    @Override
+    public boolean verify(JComponent input) {
+        JTextField txtField = (JTextField) input;
+        String valor = txtField.getText().trim();
+        if (!validarFolha(valor)) {
+            MessagesErrors.erro(component, txtField, " Folha Inválida!");
+            return false;
+        }
+        //seta cor branca
+        txtField.setBackground(Color.WHITE);
+        return true;
+    }
+
+    public static boolean validarFolha(String numero) {
+        if (numero == null) {
+            return false;
+        } else if (numero.isEmpty()) {
+            return false;
+        } //converte para um inteiro
+        else {
+            try {
+                int n = Integer.parseInt(numero);
+                if (n <= 0 || n > ProcedimentoRealizado.MAXIMA_QUANTIDADE_FOLHA) {
+                    return false;
+                }
+                return true;
+            } catch (NumberFormatException ex) {
+                //não é um número
+                return false;
+            }
         }
 
-        @Override
-            public boolean verify(JComponent input) {
-                JTextField txtField = (JTextField) input;
-                String valor = txtField.getText().trim();
-                if (valor.equals("000") || valor.isEmpty()) {  
-                     MessagesErrors.erro(component,txtField," Folha Inválida!");  
-                return false;  
-                } 
-                //seta cor branca
-                txtField.setBackground(Color.WHITE); 
-                return true;
-            }
+    }
 }
